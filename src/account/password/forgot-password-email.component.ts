d67de6538c7fd5e03512a31257465f6213018f1f
haoxj0823 <haoxj0823@139.com>
2018-01-03 21:46:47 +0800

    创建找回密码基本页面框架

---
+++ src/account/password/forgot-password-email.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    templateUrl: './forgot-password-email.component.html',
+    styleUrls: ['./forgot-password-email.component.scss']
+})
+export class ForgotPasswordEmailComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
---
+++ src/account/password/forgot-password-phone.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    templateUrl: './forgot-password-phone.component.html',
+    styleUrls: ['./forgot-password-phone.component.scss']
+})
+export class ForgotPasswordPhoneComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
