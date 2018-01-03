452c10a6656d33f309a71c0a6868c9c343fc14ba
haoxj0823 <haoxj0823@139.com>
2018-01-03 21:49:14 +0800

    添加找回密码、重置密码、用户激活组件的路由引用及登录界面的链接引用

--- src/account/account-routing.module.ts
+++ src/account/account-routing.module.ts
@@ -4,20 +4,28 @@ import { Routes, RouterModule } from '@angular/router';
 import { AccountComponent } from './account.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { ForgotPasswordEmailComponent } from './password/forgot-password-email.component';
+import { ResetPasswordComponent } from './password/reset-password.component';
+import { ForgotPasswordPhoneComponent } from './password/forgot-password-phone.component';
+import { ResetPasswordMailComponent } from './password/reset-password-mail.component';
+import { ResetPasswordSuccessComponent } from './password/reset-password-success.component';
+import { EmailActivationComponent } from './email-activation/email-activation.component';
+import { EmailActivationSuccessComponent } from './email-activation/email-activation-success.component';
 
 const routes: Routes = [
     {
         path: '',
         component: AccountComponent,
         children: [
-            {
-                path: 'login',
-                component: LoginComponent
-            },
-            {
-                path: 'register',
-                component: RegisterComponent
-            }
+            { path: 'login', component: LoginComponent },
+            { path: 'register', component: RegisterComponent },
+            { path: 'forgot-password-email', component: ForgotPasswordEmailComponent },
+            { path: 'forgot-password-phone', component: ForgotPasswordPhoneComponent },
+            { path: 'reset-password', component: ResetPasswordComponent },
+            { path: 'reset-password-mail', component: ResetPasswordMailComponent },
+            { path: 'reset-password-success', component: ResetPasswordSuccessComponent },
+            { path: 'email-activation', component: EmailActivationComponent },
+            { path: 'email-activation-success', component: EmailActivationSuccessComponent }
         ]
     },
 ];
@@ -31,5 +39,12 @@ export class AccountRoutingModule { }
 export const routedComponents = [
     AccountComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    ForgotPasswordEmailComponent,
+    ForgotPasswordPhoneComponent,
+    ResetPasswordComponent,
+    ResetPasswordMailComponent,
+    ResetPasswordSuccessComponent,
+    EmailActivationComponent,
+    EmailActivationSuccessComponent
 ];
