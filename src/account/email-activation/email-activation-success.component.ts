8fd9cae20ed65a7f8e07e9a6a55335ecd53999e7
haoxj0823 <haoxj0823@139.com>
2018-01-03 21:47:23 +0800

    创建用户激活基本页面框架

---
+++ src/account/email-activation/email-activation-success.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+    templateUrl: './email-activation-success.component.html',
+    styleUrls: ['./email-activation-success.component.scss']
+})
+export class EmailActivationSuccessComponent { }
---
+++ src/account/email-activation/email-activation.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    templateUrl: './email-activation.component.html',
+    styleUrls: ['./email-activation.component.scss']
+})
+export class EmailActivationComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
