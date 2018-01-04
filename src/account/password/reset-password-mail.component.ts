e57799cad2038100a9ec23b7bcf1599244c6721d
haoxj0823 <haoxj0823@139.com>
2018-01-04 11:28:11 +0800

    删除重置密码成功组件，采用公共成功组件

--- src/account/password/reset-password-mail.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    templateUrl: './reset-password-mail.component.html',
-    styleUrls: ['./reset-password-mail.component.scss']
-})
-export class ResetPasswordMailComponent { }
