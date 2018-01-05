cbc0404de09581de0e3abcf015446f8d21c43878
haoxj0823 <haoxj0823@139.com>
2018-01-05 14:57:01 +0800

    修改引导模块

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,33 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import './polyfills.ts';
 
-import { RootModule } from './root.module';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { enableProdMode } from '@angular/core';
 import { environment } from './environments/environment';
+import { RootModule } from './root.module';
+import { hmrBootstrap } from './hmr';
+
+import 'moment/min/locales.min';
+import 'moment-timezone';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(RootModule)
-  .catch(err => console.log(err));
+const bootstrap = () => {
+  return platformBrowserDynamic().bootstrapModule(RootModule);
+};
+
+/* "Hot Module Replacement" is enabled as described on
+ * https://medium.com/@beeman/tutorial-enable-hrm-in-angular-cli-apps-1b0d13b80130#.sa87zkloh
+ */
+
+if (environment.hmr) {
+  if (module['hot']) {
+    hmrBootstrap(module, bootstrap); // HMR enabled bootstrap
+  } else {
+    console.error('HMR is not enabled for webpack-dev-server!');
+    console.log('Are you using the --hmr flag for ng serve?');
+  }
+} else {
+  bootstrap(); // Regular bootstrap
+}
--- src/root.module.ts
+++ src/root.module.ts
@@ -1,18 +1,124 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpModule } from '@angular/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule, Injector, APP_INITIALIZER, LOCALE_ID } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+
+import { AbpModule, ABP_HTTP_PROVIDER } from '@abp/abp.module';
+
+import { AppModule } from './app/app.module';
+import { CommonModule } from '@shared/common/common.module';
+import { ServiceProxyModule } from '@shared/service-proxies/service-proxy.module';
+
+import { AppConsts } from '@shared/AppConsts';
+import { AppSessionService } from '@shared/common/session/app-session.service';
+import { API_BASE_URL } from '@shared/service-proxies/service-proxies';
+
+import { AppPreBootstrap } from './AppPreBootstrap';
+
+import { UrlHelper } from '@shared/helpers/UrlHelper';
+import { AppAuthService } from '@app/shared/common/auth/app-auth.service';
+
+import * as _ from 'lodash';
 
 import { RootComponent } from './root.component';
 import { RootRoutingModule } from './root-routing.module';
 
+export function appInitializerFactory(injector: Injector) {
+    return () => {
+        abp.ui.setBusy();
+
+        handleLogoutRequest(injector.get(AppAuthService));
+
+        return new Promise<boolean>((resolve, reject) => {
+            AppPreBootstrap.run(() => {
+                const appSessionService: AppSessionService = injector.get(AppSessionService);
+                appSessionService.init().then(
+                    (result) => {
+
+                        abp.ui.clearBusy();
+
+                        if (shouldLoadLocale()) {
+                            const angularLocale = convertAbpLocaleToAngularLocale(abp.localization.currentLanguage.name);
+                            System.import(`@angular/common/locales/${angularLocale}.js`)
+                                .then(module => {
+                                    registerLocaleData(module.default);
+                                    resolve(result);
+                                }, reject);
+                        } else {
+                            resolve(result);
+                        }
+                    },
+                    (err) => {
+                        abp.ui.clearBusy();
+                        reject(err);
+                    }
+                );
+            }, resolve, reject);
+        });
+    };
+}
+
+export function shouldLoadLocale(): boolean {
+    return abp.localization.currentLanguage.name && abp.localization.currentLanguage.name !== 'en-US';
+}
+
+export function convertAbpLocaleToAngularLocale(locale: string): string {
+    if (!AppConsts.localeMappings) {
+        return locale;
+    }
+
+    const localeMapings = _.filter(AppConsts.localeMappings, { from: locale });
+    if (localeMapings && localeMapings.length) {
+        return localeMapings[0]['to'];
+    }
+
+    return locale;
+}
+
+export function getRemoteServiceBaseUrl(): string {
+    return AppConsts.remoteServiceBaseUrl;
+}
+
+export function getCurrentLanguage(): string {
+    return abp.localization.currentLanguage.name;
+}
+
+function handleLogoutRequest(authService: AppAuthService) {
+    const currentUrl = UrlHelper.initialUrl;
+    const returnUrl = UrlHelper.getReturnUrl();
+    if (currentUrl.indexOf(('account/logout')) >= 0 && returnUrl) {
+        authService.logout(true, returnUrl);
+    }
+}
+
 @NgModule({
     imports: [
         BrowserModule,
-        HttpModule,
-        RootRoutingModule,
+        BrowserAnimationsModule,
+        AppModule,
+        CommonModule.forRoot(),
+        AbpModule,
+        ServiceProxyModule,
+
+        RootRoutingModule
     ],
-    declarations: [RootComponent],
-    providers: [],
-    bootstrap: [RootComponent],
+    declarations: [
+        RootComponent
+    ],
+    providers: [
+        ABP_HTTP_PROVIDER,
+        { provide: API_BASE_URL, useFactory: getRemoteServiceBaseUrl },
+        {
+            provide: APP_INITIALIZER,
+            useFactory: appInitializerFactory,
+            deps: [Injector],
+            multi: true
+        },
+        {
+            provide: LOCALE_ID,
+            useFactory: getCurrentLanguage
+        }
+    ],
+    bootstrap: [RootComponent]
 })
 export class RootModule { }
